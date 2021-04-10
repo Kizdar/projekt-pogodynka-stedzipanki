fee7ecdda76db0fa01fd5f456436d4b00d184204
StecLukasz <lukaszstec14@gmail.com>
2021-04-10 15:45:27 +0200

    problemy z gsonem

--- src/main/java/pogodynka/Main.java
+++ src/main/java/pogodynka/Main.java
@@ -1,9 +1,15 @@
 package pogodynka;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        String miasto = " ";
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj miasto dla którego chcesz sprawdzić pogodę");
+        miasto = input.nextLine();
 
 
 
