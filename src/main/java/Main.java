df0d69e8802537252927829c41a3a66432b39c5b
GrzegorzPanek <pgrzegorz93@gmail.com>
2021-04-11 10:19:50 +0200

    DOdanie Metod z OpenWeather

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import pogodynka.forecast.ConstantOpenWeatherForecast;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ConstantOpenWeatherForecast forecast = new ConstantOpenWeatherForecast();
+//        System.out.println(forecast.getInfo());
+//        System.out.println(forecast.getTemperature());
+//        System.out.println(forecast.getHumidity());
+        System.out.println(forecast.getPressure());
     }
 }
