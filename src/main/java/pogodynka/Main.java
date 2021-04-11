b89c777d82d92fdfd92e1b6c9d0dfdfe3de2edfe
StecLukasz <lukaszstec14@gmail.com>
2021-04-11 13:07:09 +0200

    dodawanie/wyświetlanie miejscowosci

--- src/main/java/pogodynka/Main.java
+++ src/main/java/pogodynka/Main.java
@@ -3,16 +3,22 @@ package pogodynka;
 import pogodynka.forecast.ConstantOpenWeatherForecast;
 import pogodynka.forecast.OpenWeatherLocation;
 import pogodynka.interaction.User;
+import pogodynka.model.Location;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        OpenWeatherLocation location = new OpenWeatherLocation();
         System.out.println("Witaj w programie Pogodynka - aplikacji do wyświetlania pogody :)");
         User user =  new User();
-        user.getYourChoice();
-        System.out.println("Wybrałeś pogodę dla miejscowości: " + location.getCityName());
+        Location lokalizacja = user.getYourChoice();
+        System.out.println("Wybrałeś pogodę dla miejscowości: " + lokalizacja.getName());
+
+
+
+
+
+
 
         ConstantOpenWeatherForecast weatherForecast = new ConstantOpenWeatherForecast();
         weatherForecast.getTemperature();
